c1edd1b17c2e22e0b1c65d1676e2ce359199c17b
alex <nixoved@yandex.ru>
2019-08-29 15:27:02 +0700

    springAdded

---
+++ src/main/java/radioPlayer/RadioApplication.java
@@ -0,0 +1,50 @@
+package com.nazarov.webradioplayer;
+
+import com.nazarov.webradioplayer.playlist.GitCloner;
+import org.jnativehook.GlobalScreen;
+import org.jnativehook.NativeHookException;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import com.nazarov.webradioplayer.playlist.*;
+
+@SpringBootApplication
+public class RadioApplication {
+
+    public static void main(String[] args) throws IOException {
+
+        SpringApplication.run(RadioApplication.class, args);
+
+        try {
+            GlobalScreen.registerNativeHook();
+        }
+        catch (NativeHookException ex) {
+            System.err.println("There was a problem registering the native hook.");
+            System.err.println(ex.getMessage());
+
+            System.exit(1);
+        }
+        Logger logger = Logger.getLogger(GlobalScreen.class.getPackage().getName());
+        logger.setLevel(Level.WARNING);
+
+        DeleteTemp deleteTemp = new DeleteTemp();
+        deleteTemp.del("playlists");
+
+        GitCloner g = new GitCloner();
+        g.clonePlaylists();
+
+        GenreLister genreLister = new GenreLister();
+        genreLister.logoHello();
+
+
+
+        GlobalScreen.addNativeKeyListener(new KeyController());
+
+
+
+    }
+
+}
---
+++ src/main/java/radioPlayer/WebController.java
@@ -0,0 +1,149 @@
+package com.nazarov.webradioplayer;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Controller
+@RequestMapping(path="/logo")
+public class WebController implements WebMvcConfigurer {
+
+    @RequestMapping("/ambient")
+    public void logoAmbient(HttpServletRequest request,
+                            HttpServletResponse response
+    ) throws ServletException, IOException {
+        String fileName = "ambient.mp3";
+        String dataDirectory = request.getServletContext().getRealPath("/WEB-INF/sounds/");
+        Path file = Paths.get(dataDirectory, fileName);
+        if (Files.exists(file)) {
+            response.setContentType("audio/mpeg");
+            //  response.addHeader("Content-Disposition", "attachment; filename="+fileName);
+
+            try {
+                Files.copy(file, response.getOutputStream());
+                response.getOutputStream().flush();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    @RequestMapping("/jazz")
+    public void logoJazz(HttpServletRequest request,
+                         HttpServletResponse response
+    ) throws ServletException, IOException {
+        String fileName = "jazz.mp3";
+        String dataDirectory = request.getServletContext().getRealPath("/WEB-INF/sounds/");
+        Path file = Paths.get(dataDirectory, fileName);
+        if (Files.exists(file)) {
+            response.setContentType("audio/mpeg");
+            //  response.addHeader("Content-Disposition", "attachment; filename="+fileName);
+
+            try {
+                Files.copy(file, response.getOutputStream());
+                response.getOutputStream().flush();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    @RequestMapping("/trance")
+    public void logoTrance(HttpServletRequest request,
+                           HttpServletResponse response
+    ) throws ServletException, IOException {
+        String fileName = "trance.mp3";
+        String dataDirectory = request.getServletContext().getRealPath("/WEB-INF/sounds/");
+        Path file = Paths.get(dataDirectory, fileName);
+        if (Files.exists(file)) {
+            response.setContentType("audio/mpeg");
+            //  response.addHeader("Content-Disposition", "attachment; filename="+fileName);
+
+            try {
+                Files.copy(file, response.getOutputStream());
+                response.getOutputStream().flush();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    @RequestMapping("/rock")
+    public void logoRock(HttpServletRequest request,
+                         HttpServletResponse response
+    ) throws ServletException, IOException {
+        String fileName = "retro.mp3";
+        String dataDirectory = request.getServletContext().getRealPath("/WEB-INF/sounds/");
+        Path file = Paths.get(dataDirectory, fileName);
+        if (Files.exists(file)) {
+            response.setContentType("audio/mpeg");
+            //  response.addHeader("Content-Disposition", "attachment; filename="+fileName);
+
+            try {
+                Files.copy(file, response.getOutputStream());
+                response.getOutputStream().flush();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    @RequestMapping("/other")
+    public void logoOther(HttpServletRequest request,
+                          HttpServletResponse response
+    ) throws ServletException, IOException {
+        String fileName = "other.mp3";
+        String dataDirectory = request.getServletContext().getRealPath("/WEB-INF/sounds/");
+        Path file = Paths.get(dataDirectory, fileName);
+        if (Files.exists(file)) {
+            response.setContentType("audio/mpeg");
+            //  response.addHeader("Content-Disposition", "attachment; filename="+fileName);
+
+            try {
+                Files.copy(file, response.getOutputStream());
+                response.getOutputStream().flush();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    @RequestMapping("/hello")
+    public void logoHello(HttpServletRequest request,
+                          HttpServletResponse response
+    ) throws ServletException, IOException {
+        String fileName = "hello.mp3";
+        String dataDirectory = request.getServletContext().getRealPath("/WEB-INF/sounds/");
+        Path file = Paths.get(dataDirectory, fileName);
+        if (Files.exists(file)) {
+            response.setContentType("audio/mpeg");
+            //  response.addHeader("Content-Disposition", "attachment; filename="+fileName);
+
+            try {
+                Files.copy(file, response.getOutputStream());
+                response.getOutputStream().flush();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
